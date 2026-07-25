89a47b2947cc322cec1b217fcd7a112483140050
moerukouri <twebblemc@gmail.com>
2026-07-25 16:04:02 +0800

    fix improved loop system and return input

--- InputValidator.java
+++ InputValidator.java
@@ -4,19 +4,18 @@ public class InputValidator {
     private final Scanner sc;
 
     public InputValidator() {
-        this.sc = scanner;
+        this.sc = new Scanner(System.in);
     }
 
-    public static String getValidatedInput(String prompt, String regex, String errorMsg) {
-        boolean running = true;
-        while (running) {
-            System.out.print(prompt);
-            String input = sc.nextLine().trim();
-            if (input.matches(regex)) {
-                return input;
-            }
-            System.out.println(errorMsg);
-            running = false;
+    public String getValidatedInput(String prompt, String regex, String errorMsg) {
+    System.out.print(prompt);
+    String input = sc.nextLine().trim();
+
+    while (!input.matches(regex)) {
+        System.out.println(errorMsg);
+        System.out.print(prompt);
+        input = sc.nextLine().trim();
         }
+        return input;
     }
 }
